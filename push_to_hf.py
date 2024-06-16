d98a3d3e0726f53712a80315f8aaf34d0db1b850
Niels <niels.rogge1@gmail.com>
2024-06-16 11:14:06 +0200

    First draft

---
+++ push_to_hf.py
@@ -0,0 +1,31 @@
+from guided_diffusion.models import Model
+import torch
+import yaml
+from utils import dict2namespace
+
+
+def load_pretrained_diffusion_model(config):
+    model = Model(config)
+    ckpt = "checkpoints/celeba_hq.ckpt"
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    config.device = device
+    model.load_state_dict(torch.load(ckpt, map_location=device))
+    model.to(device)
+    model.eval()
+    for param in model.parameters():
+        param.requires_grad = False
+    return model, device
+
+
+with open( "data/celeba_hq.yml", "r") as f:
+    config1 = yaml.safe_load(f)
+
+config = dict2namespace(config1)
+
+model, device = load_pretrained_diffusion_model(config)
+
+# push to hub
+model.push_to_hub("cvg-unibe/bird-celeba-hq")
+
+# reload
+model = Model.from_pretrained("cvg-unibe/bird-celeba-hq")
